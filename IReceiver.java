e6900341b94b3b7e6444d64c1dc598fccd1112e9
PhamNhatLinh1998 <43946910+PhamNhatLinh1998@users.noreply.github.com>
2018-12-22 14:39:51 +0700

    Add files via upload

--- IReceiver.java
+++ IReceiver.java
@@ -1,3 +1,5 @@
+package myjava.mybignumber;
+
 /**
  * DesCription.
  * IReceiver là interface cho phép in từng bước cộng 2 số
@@ -6,12 +8,10 @@
 public interface IReceiver {
 
     /*
-     * Để thực hiện việc in từng bước cộng 2 chuỗi số
-     * thì lớp nào implements interface này thì phải hiện thực
-     * hàm send của IReceiver
-     * <br/>
-     *
-     */
+	 * Để thực hiện việc in từng bước cộng 2 chuỗi số thì lớp nào implements
+	 * interface này thì phải hiện thực hàm send của IReceiver <br/>
+	 *
+	 */
 
-    public abstract void send(String msg);
+	public abstract void send(String msg);
 }
